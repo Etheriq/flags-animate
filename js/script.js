106dee4fdabd66991dc3b5eab914464e06931c05
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-03-22 21:39:46 +0200

    in progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
-    var flags = $(".flags li");
+//    var flags = $(".flags li");
+    var flags = $(".flags-ul-list").children();
 
-    $(".round-left").click(function(){
+    console.dir(flags);
+    var liCount = $(".flags-ul-list").children().length;
+    var containerWidth = $(".flags-ul-list").width();
+    var liWidth = containerWidth / liCount;
 
-        $(flags).get(0).appendTo(".list-flags");
+    var x = 0;
+    flags.each( function(index) {
+
+        $(this).css(
+            "left", x
+        );
+        x += $(this).outerWidth(true);
+    });
+
+$(".round-left").click(function(){
+    var flags = $(".flags-ul-list").children();
+    var firstLi = flags.first();
+    var lastLi = flags.last();
+    var x = firstLi.outerWidth(true);
+    var firstLiClone = firstLi.clone();
+
+    console.log("x= " + x);
+    flags.first().remove();
+    for(var i= 0; i<flags.length; i++) {
+        $(flags[i]).animate({
+                left: '-='+x
+            },
+            1000);
+    }
+    $(firstLiClone).css("left", $(lastLi).css("left"));
+    $(".flags-ul-list").append(firstLiClone);
+
+//
+//        flags.each(function(index){
+//
+//            $(this).css("left", "-=x");
+//        });
+
+//        flags.append(firstLi);
+//        $(flags).get(0).appendTo(".list-flags");
 
 
 //        flags.each(function(index, domEl){
@@ -16,5 +54,6 @@ $(document).ready(function(){
 //            },
 //            1000);
 //        });
-    });
+});
+
 });
