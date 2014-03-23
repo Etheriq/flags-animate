34d13ef37c07e9e88cc68b3a07f3bf1b82896f8c
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-03-23 22:52:29 +0200

    round left ready

--- js/script.js
+++ js/script.js
@@ -1,59 +1,91 @@
 $(document).ready(function(){
 //    var flags = $(".flags li");
     var flags = $(".flags-ul-list").children();
+    var liCount = flags.children().length;
 
-    console.dir(flags);
-    var liCount = $(".flags-ul-list").children().length;
-    var containerWidth = $(".flags-ul-list").width();
-    var liWidth = containerWidth / liCount;
+    var maxWidthArray = [];
+    for(var i = 0; i<liCount; i++) {
+        maxWidthArray.push(($(flags[i]).outerWidth(true))+20);
+    }
 
+    var maxWidthLi = Math.max.apply({}, maxWidthArray);
+    console.log(maxWidthArray);
+    console.log(maxWidthLi);
+    $(flags).css("width", maxWidthLi);
+
+    var containerWidth = $(".flags").width();
+    var liLeftStep = containerWidth / liCount;
     var x = 0;
     flags.each( function(index) {
 
         $(this).css(
             "left", x
         );
-        x += $(this).outerWidth(true);
+        x += liLeftStep;
     });
 
 $(".round-left").click(function(){
     var flags = $(".flags-ul-list").children();
     var firstLi = flags.first();
+    var firstLiWidth = firstLi.outerWidth(true);
+    var firstLiCloned = firstLi.clone();
     var lastLi = flags.last();
-    var x = firstLi.outerWidth(true);
-    var firstLiClone = firstLi.clone();
+    var z = parseInt($(lastLi).css("left"));
 
-    console.log("x= " + x);
-    flags.first().remove();
-    for(var i= 0; i<flags.length; i++) {
-        $(flags[i]).animate({
-                left: '-='+x
+    var callbacks = $.Callbacks();
+
+    function firstLiDo()
+    {
+        $(firstLi).animate({
+                opacity: 0,
+                left: -100
+            },
+            1000,
+            function(){
+                firstLi.remove();
+            });
+    }
+
+    function otherLiDo()
+    {
+        $(flags).each(function(index){
+            $(this).animate({
+                    left: '-='+firstLiWidth
+                },
+                1000)
+        });
+    }
+
+    function lastLiDo()
+    {
+        z += 100;
+        $(firstLiCloned).css({
+            "left": z,
+            "opacity": 0
+        });
+
+        $(".flags-ul-list").append(firstLiCloned);
+
+        z -= 100;
+        $(firstLiCloned).animate({
+                opacity:1,
+                left: z
             },
             1000);
     }
-    $(firstLiClone).css("left", $(lastLi).css("left"));
-    $(".flags-ul-list").append(firstLiClone);
 
-//
-//        flags.each(function(index){
-//
-//            $(this).css("left", "-=x");
-//        });
+    callbacks.add(firstLiDo());
+    callbacks.add(otherLiDo());
+    callbacks.add(lastLiDo());
 
-//        flags.append(firstLi);
-//        $(flags).get(0).appendTo(".list-flags");
+    callbacks.fire();
 
+    callbacks.empty();
 
-//        flags.each(function(index, domEl){
+});
 
-
-
-//            $(domEl).animate({
-//                left : '-=40px'
-////                    opacity: 'hide'
-//            },
-//            1000);
-//        });
+$(".round-right").click(function(){
+    console.log('right');
 });
 
 });
